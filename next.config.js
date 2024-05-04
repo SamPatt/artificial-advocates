70fad2380743f08a7adb323ecfacfea4aaf651ac
SamPatt <git@sampatt.com>
2024-05-03 22:25:48 -0400

    Initial commit; follows Replicate Next.js tutorial

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'replicate.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'replicate.delivery',
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
